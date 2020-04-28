1e305405f69bac0e1cea5c59d62d9175022790c5
Tongbo Zhang <ztb5129@live.com>
2020-04-28 17:17:23 +0800

    Add wechat support

--- submit.py
+++ submit.py
@@ -2,19 +2,63 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import itertools
+import requests
 import time
+import json
 
 # personal info
 username = "XXX"
 password =  "XXX"
+logpath = r""
+
+# Push
+secret = ""
+corpID = ""
+agentID = ""
+pushuser = ""
+flag = False
+
+
+def write_log(logstr):
+    global logpath
+    if logpath != "":
+        with open (logpath,'a+')as f:
+            f.write (logstr + "\n")
+        f.close()
+
+def push_wechat():
+    global flag, secret, corpID, agentID
+    tokenUrl = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={}&corpsecret={}"
+    postUrl = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}"
+    req = requests.get(url=tokenUrl.format(corpID,secret))
+    acctoken = req.json().get('access_token')
+    constr = "{} Success".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    if not flag:
+        constr = constr.replace("Success", "Failure")
+    pushdata = {
+        "touser" : pushuser,
+        "msgtype" : "text",
+        "agentid" : agentID,
+        "text" : {
+            "content" : constr
+        },
+        "safe":0
+    }
+    ret = requests.post(url=postUrl.format(acctoken),data=json.dumps(pushdata))
+    if ret.json()["errcode"] == 0:
+        write_log("WeChat pushed successfully: " + constr)
+    else:
+        write_log("WeChat pushed failed: " + constr)
+        write_log("Push error: " + str(ret.json()))
 
 def submit_form(count):
+    global flag, username, password, logpath
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--log-level=3")
+    #chrome_options.add_argument("--log-level=3")
     browser = webdriver.Chrome(options = chrome_options)
     browser.set_page_load_timeout(1000)
-    flag = False
     try:
         # home page
         browser.get("https://ehall.jlu.edu.cn/")
@@ -26,6 +70,9 @@ def submit_form(count):
         browser.get("https://ehall.jlu.edu.cn/infoplus/form/JLDX_YJS_XNYQSB/start")
         # check checkbox & submit
         WebDriverWait(browser,60,2).until(EC.presence_of_element_located((By.NAME,"fieldCNS")))
+        #ele = browser.find_element_by_xpath('/html/body/div[4]/form/div/div[2]/div[3]/div/div[1]/div[1]/table/tbody/tr[2]/td/div/table/tbody/tr[34]/td[4]/div/div/div/div/input')
+        #ele.send_keys("上海市")
+        #ele.send_keys(Keys.ENTER)
         browser.find_element_by_name("fieldCNS").click()
         browser.find_element_by_xpath("//*[@id='form_command_bar']/li[1]/a[1]").click()
         # pop-up window
@@ -35,14 +82,16 @@ def submit_form(count):
         time.sleep(5)
         if browser.page_source.find("办理成功") != -1:
             flag = True
-    except:
-        print("Error!")
+    except Exception as e:
+        print(e)
+        write_log("Submit error: " + str(e))
     finally:
-        if flag:
-            print ("Submission {}: Success".format(count))
-        else:
-            print ("Submission {}: Failure".format(count))
         browser.close()
+        logstr = "{} Submission {}: Success".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), count)
+        if not flag:
+            logstr = logstr.replace("Success", "Failure")
+        write_log(logstr)
+        print(logstr)
         return flag
 
 if __name__=="__main__":
@@ -51,3 +100,5 @@ if __name__=="__main__":
         count += 1
         if submit_form(count):
             break
+    if secret != "":
+        push_wechat()
